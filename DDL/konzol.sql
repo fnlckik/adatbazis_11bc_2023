8452fea253603ccdbc93fea31e308a50d657cb84
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-17 12:24:01 +0200

    konzol.sql

---
+++ DDL/konzol.sql
@@ -0,0 +1,28 @@
+-- Egy soros komment
+/*
+    Több soros megjegyzés
+*/
+
+-- Adatbázisok listázása
+SHOW DATABASES;
+
+-- Adatbázis kiválasztása
+USE information_schema;
+
+-- Táblák listázása
+SHOW TABLES;
+
+-- Tábla teljes tartalmának listázása
+SELECT * FROM character_sets;
+
+------------------------------------------------
+
+-- MySQL verzió szám: C:\xampp\mysql\bin
+-- mysql --version
+
+-- Bejelentkezés (cmd-ben)
+mysql -u root
+
+-- Kijelentkezés (cmd-ben)
+exit
+
