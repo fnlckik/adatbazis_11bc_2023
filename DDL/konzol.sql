6dcc646e1a94a350d85329bf86bf2de6c09ad169
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-19 09:45:34 +0200

    konzol.sql

---
+++ DDL/konzol.sql
@@ -0,0 +1,15 @@
+/*
+    Több soros
+    komment
+*/
+
+-- Egy soros komment
+-- Adatbázisok listázása
+SHOW DATABASES;
+
+-- Adatbázis használata
+USE information_schema;
+
+-- Táblák listázása
+SHOW TABLES;
+
