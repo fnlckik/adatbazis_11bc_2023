e63ae420a8101c16c8bce3f1dde42b5301d998b2
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-24 13:26:08 +0200

    CREATE TABLE alapok

--- DDL/konzol.sql
+++ DDL/konzol.sql
@@ -13,3 +13,15 @@ USE information_schema;
 -- Táblák listázása
 SHOW TABLES;
 
+-- MySQL konzol
+-- Verzió ellenőrzés: mysql --version
+
+-- Bejelentkezés
+mysql -u root -p
+
+-- Kijelentkezés
+exit
+
+-- Tábla tartalmának listázása
+SELECT * FROM character_sets;
+
---
+++ DDL/tablak.sql
@@ -0,0 +1,13 @@
+-- Adatbázis létrehozása
+CREATE DATABASE iskola_cs1;
+
+-- Adatbázis törlése
+DROP DATABASE iskola_cs1;
+
+-- Tábla létrehozása
+CREATE TABLE diak (
+    id INT,
+    nev VARCHAR(25),
+    kor TINYINT UNSIGNED
+);
+
