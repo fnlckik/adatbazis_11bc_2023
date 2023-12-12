f4fb90eed81ba3ca07054fcf159ff8b1f4d2b2c0
farkas.norbert <farkas.norbert@ckik.hu>
2023-12-12 12:40:45 +0100

    Rendelesek alap

---
+++ DML/Rendelések/rendeles.sql
@@ -0,0 +1,43 @@
+-- 1. Rendelés tábla
+
+
+-- 2. Rendelés adatok (8 rekord)
+INSERT INTO Rendeles (id, ugyfel_id, idopont, allapot, osszeg, valuta)
+VALUES 
+(1, 'User123', '2023-12-11 08:15', 'rögzítve', 15000, 'forint'),
+(2, 'GreenDragon', '2023-12-11 09:30', 'folyamatban', 20000, 'euró'),
+(3, 'Sunflower22', '2023-12-11 10:45', 'teljesítve', 25000, 'font'),
+(4, 'GreenDragon', '2023-12-11 12:00', 'teljesítve', 30000, 'dollár'),
+(5, 'SilverFox', '2023-12-11 13:15', 'folyamatban', 35000, 'forint'),
+(6, 'GreenDragon', '2023-12-11 14:30', 'rögzítve', 40000, 'euró'),
+(7, 'BlueBird', '2023-12-11 15:45', 'rögzítve', 45000, 'font'),
+(8, 'MountainView', '2023-12-11 17:00', 'törölve', 50000, 'dollár');
+
+-------------------------------------------------------------------------------
+-- Gyakorlás
+-------------------------------------------------------------------------------
+
+-- 1.
+
+
+-- 2.
+
+
+-- 3.
+
+
+-- 4.
+
+
+-- 5.
+
+
+-- 6.
+
+
+-- 7.
+
+
+-- 8.
+
+
