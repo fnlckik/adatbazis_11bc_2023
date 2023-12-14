b3a1f71e5c938d324fa6f6632623e34619bde265
farkas.norbert <farkas.norbert@ckik.hu>
2023-12-14 09:41:24 +0100

    DELETE, LIKE

--- DML/Rendelések/ugyfel.sql
+++ DML/Rendelések/ugyfel.sql
@@ -20,17 +20,49 @@ FROM ugyfel
 WHERE fnev = "SkyWalker89";
 
 -- 2.
-
+DELETE
+FROM ugyfel
+WHERE fizetes < 300000;
 
 -- 3.
-
+DELETE
+FROM ugyfel
+WHERE varos IN ("Eger", "Kaposvár", "Budapest");
 
 -- 4.
+-- Mintaillesztés
+-- Joker karakter:
+-- %: valahány karakter (akár 0 darab is)
+-- _: pontosan 1 karakter
+DELETE
+FROM ugyfel
+WHERE email LIKE "%ckik.hu";
 
+-- Amiben van pont az email felhasználóban
+SELECT *
+FROM ugyfel
+WHERE email LIKE "%.%@%";
+
+-- Amiben 5 karakter után van pont az email felhasználóban
+SELECT *
+FROM ugyfel
+WHERE email LIKE "_____.%";
+-- LIKE "_%@_%._%"
+-- Legalább 1 karakter: "_%"
+-- LIKE mehet CHECK feltételbe is
 
 -- 5.
+DELETE
+FROM ugyfel
+WHERE szuletes LIKE "1983%";
 
+SELECT *
+FROM ugyfel
+WHERE YEAR(szuletes) = 1983;
 
+SELECT *
+FROM ugyfel
+WHERE "1983-01-01" <= szuletes AND szuletes <= "1983-12-31";
 
 ---------------------------------------------------------------------------------
 -- UPDATE (eredeti adatokkal)
