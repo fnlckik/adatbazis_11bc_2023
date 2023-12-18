041bd748886a1d1c421fdcdafbe5708d9b5855a3
farkas.norbert <farkas.norbert@ckik.hu>
2023-12-18 08:46:22 +0100

    UPDATE

--- DML/Rendelések/ugyfel.sql
+++ DML/Rendelések/ugyfel.sql
@@ -61,20 +61,44 @@ WHERE "1983-01-01" <= szuletes AND szuletes <= "1983-12-31";
 ---------------------------------------------------------------------------------
 
 -- 1.
+-- SELECT *
+-- FROM ugyfel
+-- WHERE fnev = "GamerSpirit";
 
+UPDATE ugyfel
+SET varos = "Budapest"
+WHERE fnev = "GamerSpirit";
 
 -- 2.
-
+UPDATE ugyfel
+SET fizetes = fizetes + 5999
+WHERE nem = "N";
 
 -- 3.
-
+UPDATE ugyfel
+SET fizetes = fizetes * 3
+WHERE varos = "Budapest";
 
 -- 4.
-
+UPDATE ugyfel
+SET fizetes = fizetes * 1.035
+WHERE telefon LIKE "3670%";
 
 -- 5.
+-- UPDATE ugyfel
+-- SET varos = "Cegléd"
+-- WHERE email LIKE "%@ckik.hu" AND nem = "F";
 
+-- UPDATE ugyfel
+-- SET fizetes = NULL
+-- WHERE email LIKE "%@ckik.hu" AND nem = "F";
+
+UPDATE ugyfel
+SET varos = "Cegléd", fizetes = NULL
+WHERE email LIKE "%@ckik.hu" AND nem = "F";
 
 -- 6.
-
+UPDATE ugyfel
+SET telefon = NULL
+WHERE telefon LIKE "%7_";
 
