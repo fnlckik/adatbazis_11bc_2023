c9b1d95a2acf257713fe0c7bec2833fa7b3c257b
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-12 11:44:37 +0100

    Edzés (feladat)

---
+++ Dolgozat/Edzés/edzesek.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE Edzesek;
+USE Edzesek;
+
+CREATE TABLE Edzo (
+    nev VARCHAR(20) PRIMARY KEY,
+    telefon CHAR(11),
+    oradij INT,
+    ertekeles FLOAT,
+    szuletes DATE,
+    szakag VARCHAR(50),
+    aktivitas BOOLEAN
+);
+
+INSERT INTO Edzo (nev, telefon, oradij, ertekeles, szuletes, szakag, aktivitas)
+VALUES
+('Bob Johnson', '06701234567', 5500, 4.8, '1982-08-10', 'yoga', false),
+('Jane Smith', '06301234567', 4500, 4.0, '1990-02-28', 'aerobik', true),
+('Yannis Mitchell', '06706543210', 4400, 3.9, '1995-03-07', 'pilates', false),
+('Grace Harris', '06708901234', 5200, 4.7, '1993-06-18', 'aerobik', true),
+('Samuel Murphy', '06705432109', 5300, 4.6, '1989-12-03', 'crossfit', true),
+('David White', '06705678901', 6000, 4.9, '1975-09-22', 'sprint', true),
+('Eva Miller', '06206789012', 3700, 4.5, '1998-07-14', 'tánc', true),
+('Frank Davis', '06307890123', 4200, 4.1, '1980-12-05', 'fitness', false),
+('Tina Collins', '06203216549', 3900, 3.5, '1992-05-20', 'sprint', true),
+('Walter Bennett', '06204321098', 5000, 4.3, '1986-06-10', 'aerobik', false),
+('Harry Martin', '06209012345', 4800, 4.3, '1987-03-25', 'yoga', false),
+('Jack Brown', '06702345678', 4000, 3.6, '1996-09-30', 'crossfit', true),
+('Karen Clark', '06203456789', 4900, 4.4, '1981-01-12', 'sprint', false),
+('Liam Johnson', '06304567890', 5100, 4.6, '1986-07-08', 'tánc', true),
+('Mia Wilson', '06705678901', 3800, 3.9, '1999-04-26', 'fitness', true),
+('Noah White', '06206789012', 3700, 4.2, '1994-11-15', 'aerobik', false),
+('Olivia Davis', '06307890123', 5700, 4.5, '1977-02-09', 'yoga', true),
+('Parker Harris', '06708901234', 4600, 4.2, '1984-06-22', 'pilates', false),
+('Quentin Turner', '06209876543', 4800, 4.1, '1983-04-12', 'pilates', true),
+('Rachel Foster', '06307654321', 4100, 3.8, '1997-08-27', 'kardio', false),
+('Ivy Taylor', '06301234567', 5500, 4.8, '1982-08-10', 'pilates', true),
+('Alice Brown', '06203456789', 4000, 3.7, '1995-11-20', 'kardio', true),
+('John Doe', '06201234567', 5000, 4.5, '1985-05-15', 'fitness', true),
+('Ulysses Rodriguez', '06309871234', 5600, 4.7, '1978-10-15', 'tánc', false),
+('Victoria Turner', '06706789012', 4200, 4.0, '1984-02-28', 'fitness', true),
+('Charlie Wilson', '06304567890', 4800, 4.2, '1988-04-03', 'crossfit', false),
+('Xena Lopez', '06305432109', 4700, 4.2, '1991-09-14', 'yoga', true),
+('Zara Parker', '06207654321', 5400, 4.8, '1980-11-22', 'kardio', true);
+
+-- F1
+
+
+-- F2
+
+
+-- F3
+
+
+-- F4
+
+
+-- F5
+
+
+-- F6
+
+
+-- F7
+
+
+-- F8
+
+
+-- F9
+
+
+-- F10
+
