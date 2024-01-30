23438252b723d34355b13557bb1d2c8154e1e46c
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-30 12:26:53 +0100

    CASE, IS NULL, IS NOT NULL, COALESCE

--- DQL/Dolgozók/dolgozok.sql
+++ DQL/Dolgozók/dolgozok.sql
@@ -41,29 +41,70 @@ FROM Dolgozok;
 -- F5
 -- Soroljuk 3 kategóriába kereset szerint az embereket:
 -- 0-500000 szegény; 500001-800000 átlagos; 800000- gazdag
-
+SELECT 
+    vnev, knev, fizetes, 
+    IF (fizetes > 800000, "gazdag", 
+        IF (fizetes > 500000, "átlagos", "szegény")
+    ) AS Kategoria
+FROM Dolgozok;
 
 -- Másképp: esetekre bontás
-
+SELECT 
+    vnev, knev, fizetes, 
+    CASE
+        WHEN fizetes > 800000 THEN "gazdag"
+        WHEN fizetes > 500000 THEN "átlagos"
+        ELSE "szegény"
+    END AS Kategoria
+FROM Dolgozok;
 
 -- F6
 -- Add meg azokat, akiknek nem tudjuk a nemét!
+-- SELECT vnev, knev, nem
+-- FROM Dolgozok
+-- WHERE nem = NULL;
+SELECT vnev, knev, nem
+FROM Dolgozok
+WHERE nem IS NULL;
 
 
 -- F7
 -- Add meg azokat, akiknek tudjuk az email címét!
+-- SELECT id, vnev, knev, email
+-- FROM Dolgozok
+-- WHERE NOT(email IS NULL);
+
+SELECT id, vnev, knev, email
+FROM Dolgozok
+WHERE email IS NOT NULL;
 
 
 -- F8
 -- Akiknek nem tudjuk a nemét, azoknak a neme helyett "-" jelenjen meg!
+SELECT vnev, knev, IF(nem IS NULL, "-", nem) nem
+FROM Dolgozok;
 
+SELECT vnev, knev, IF(nem IS NOT NULL, nem, "-") nem
+FROM Dolgozok;
+
+SELECT vnev, knev, IFNULL(nem, "-") nem
+FROM Dolgozok;
 
 -- Másik megoldás: egyesülés
+SELECT vnev, knev, COALESCE(nem, "-") nem
+FROM Dolgozok;
 
+-- Kitekintés:
+-- COALESCE => legelső paraméter értékét adja vissza, ami már nem NULL
+SELECT id, COALESCE(jutalom, fizetes*0.01, 5000)
+FROM Dolgozok;
 
 -- F9
 -- Add meg a dolgozók teljes keresetét!
-
+SELECT
+    vnev, knev, fizetes, jutalom,
+    jutalom + fizetes AS Kereset
+FROM Dolgozok;
 
 -- F10
 -- Add meg a jutalom hány százaléka a fizetésnek!
---
+++ DQL/Dolgozók/elagazas.py
@@ -0,0 +1,15 @@
+fizetes = 550000
+# if fizetes > 800000:
+#     print("gazdag")
+# elif fizetes > 500000:
+#     print("átlagos")
+# else:
+#     print("szegény")
+
+if fizetes > 800000:
+    print("gazdag")
+else:
+    if fizetes > 500000:
+        print("átlagos")
+    else:
+        print("szegény")
