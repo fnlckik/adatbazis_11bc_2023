31954729f0e5a5b4bfba052c346ee46684edcdeb
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-01 09:43:09 +0100

    CASE, IS NULL, COALESCE

--- DQL/Dolgozók/dolgozok.sql
+++ DQL/Dolgozók/dolgozok.sql
@@ -37,25 +37,95 @@ FROM Dolgozok;
 
 -- F5
 -- Soroljuk 3 kategóriába kereset szerint az embereket:
--- 0-500000 szegény; 500001-800000 átlagos; 800000- gazdag
-
+-- -500000 szegény; 500001-800000 átlagos; 800000- gazdag
+SELECT 
+    vnev, knev, fizetes,
+    IF(fizetes > 800000, 
+        "gazdag",
+        IF(fizetes > 500000,
+            "átlagos",
+            "szegény"
+        )
+    ) AS Kategoria
+FROM Dolgozok;
 
 -- Másképp: esetekre bontás
-
+SELECT
+    vnev, knev, fizetes,
+    CASE
+        WHEN fizetes > 800000 THEN "gazdag"
+        WHEN fizetes > 500000 THEN "átlagos"
+        ELSE "szegény"
+    END
+FROM Dolgozok;
 
 -- F6
 -- Add meg azokat, akiknek nem tudjuk a nemét!
 
+-- Rossz!
+-- SELECT vnev, knev, nem
+-- FROM Dolgozok
+-- WHERE nem = NULL;
+
+-- Rossz!
+-- SELECT vnev, knev, nem
+-- FROM Dolgozok
+-- WHERE nem <> "F" AND nem <> "N";
+
+-- Rossz!
+-- SELECT vnev, knev, nem
+-- FROM Dolgozok
+-- WHERE NOT nem;
+
+SELECT vnev, knev, nem
+FROM Dolgozok
+WHERE nem IS NULL;
 
 -- F7
 -- Add meg azokat, akiknek tudjuk az email címét!
+SELECT vnev, knev, email
+FROM Dolgozok
+WHERE NOT(email IS NULL);
 
+SELECT vnev, knev, email
+FROM Dolgozok
+WHERE email IS NOT NULL;
+
+SELECT vnev, knev, email
+FROM Dolgozok
+WHERE NOT email;
+
+-- Kitekintés: Vigyázat, számoknál utóbbi nem jó!
+-- Kik azok akiknek tudjuk a jutalmát?
+SELECT vnev, knev, jutalom
+FROM Dolgozok
+WHERE NOT jutalom;
 
 -- F8
 -- Akiknek nem tudjuk a nemét, azoknak a neme helyett "-" jelenjen meg!
+SELECT 
+    vnev, knev,
+    IF(nem IS NULL, "-", nem) AS nem
+FROM Dolgozok;
 
+SELECT 
+    vnev, knev,
+    IFNULL(nem, "-") AS nem
+FROM Dolgozok;
 
--- Másik megoldás: egyesülés
+-- Másik megoldás: egyesülés - COALESCE
+-- Legelső nem NULL értéket veszi ki!
+SELECT 
+    vnev, knev,
+    COALESCE(nem, "-") AS nem
+FROM Dolgozok;
+
+-- Ha nem tudjuk a jutalmat, legyen a fizetés 1%-a
+-- Ha nem tudjuk a fizetést sem, legyen fixen 5000
+SELECT
+    vnev, knev, fizetes,
+    COALESCE(jutalom, fizetes * 0.01, 5000) AS jutalom
+FROM Dolgozok;
 
 
 -- F9
