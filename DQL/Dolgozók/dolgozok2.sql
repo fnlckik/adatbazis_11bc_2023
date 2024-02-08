6f094445527f32e3626bf892c2df9d96e961c3b2
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-08 09:40:50 +0100

    YEAR, MONTH, LPAD, CURDATE, CURTIME

--- DQL/Dolgozók/dolgozok2.sql
+++ DQL/Dolgozók/dolgozok2.sql
@@ -5,7 +5,7 @@
 SELECT
     vnev, knev, szuletes,
     DATE_FORMAT(szuletes, "%Y. %m. %d.") AS sz_datum,
-    DATE_FORMAT(szuletes, "%Y. %M %d.", ???) AS sz_datum2
+    DATE_FORMAT(szuletes, "%Y. %M %d., %W", "hu_HU") AS sz_datum2
 FROM Dolgozok;
 
 
@@ -13,27 +13,80 @@ FROM Dolgozok;
 
 -- F2
 -- Add meg a dolgozók születési évét, hónapját és napját!
-
+SELECT 
+    vnev, knev, szuletes,
+    YEAR(szuletes) AS év,
+    MONTH(szuletes) AS hónap,
+    DAY(szuletes) AS nap
+FROM Dolgozok;
 
 
 
 
 -- F3
 -- Add meg a dolgozók születési hónapját vezető 0-kal kiegészítve!
-
+SELECT 
+    vnev, knev, szuletes,
+    IF(MONTH(szuletes) < 10,
+        CONCAT("0", MONTH(szuletes)),
+        MONTH(szuletes)
+    ) AS hónap
+FROM Dolgozok;
 
 -- Másképp: bal oldali "tömés"
+-- LPAD: left padding
+-- LPAD(Mit?, Milyen hosszura?, Milyen karakterekkel?)
+-- SELECT LPAD("alma", 12, "#"), LPAD("cseresznye", 12, "#");
+-- RPAD: right padding
+-- SELECT RPAD(1200.05, 10, 0), RPAD(200.05, 10, 0);
+SELECT 
+    vnev, knev, szuletes,
+    LPAD(MONTH(szuletes), 2, 0) AS hónap
+FROM Dolgozok;
 
 
 -- Másképp dátum formátum?
+-- 2024. 02. 08.
+SELECT 
+    vnev, knev, szuletes,
+    CONCAT(
+        YEAR(szuletes), ". ",
+        LPAD(MONTH(szuletes), 2, 0), ". ",
+        LPAD(DAY(szuletes), 2, 0), "."
+    ) AS sz_datum
+FROM Dolgozok;
 
+SELECT 
+    vnev, knev, szuletes,
+    CONCAT(
+        YEAR(szuletes),
+        ". ",
+        IF(MONTH(szuletes) < 10,
+            CONCAT("0", MONTH(szuletes)),
+            MONTH(szuletes)
+        ),
+        ". ",
+        IF(DAY(szuletes) < 10,
+            CONCAT("0", DAY(szuletes)),
+            DAY(szuletes)
+        ),
+        "."
+    ) AS sz_datum,
+    DATE_FORMAT(szuletes, "%Y. %m. %d.") AS sz_datum2
+FROM Dolgozok;
 
 
 
 
 -- F4
 -- Add meg a mai napot és az aktuális időt (ó, p, mp)!
-
+-- CURDATE(): current date => DATE
+-- CURTIME(): current time => TIME
+-- ???
+SELECT
+    CURDATE() AS dátum,
+    CURTIME() AS idő,
+    CONCAT(CURDATE(), " ", CURTIME()) AS dátum_idő;
 
 
 
