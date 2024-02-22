1d268aa75fa4485edfd7826211b996c44756b27f
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-22 09:40:23 +0100

    DISTINCT

--- DQL/Dolgozók/dolgozok3.sql
+++ DQL/Dolgozók/dolgozok3.sql
@@ -49,23 +49,45 @@ FROM Dolgozok;
 
 -- 4. Add meg a "/" karaktert tartalmazó telefonszámok lényegi (utolsó 7) számjegyét!
 SELECT vnev, telefonszam, RIGHT(telefonszam, 8)
-FROM Dolgozok;
+FROM Dolgozok
+WHERE telefonszam LIKE "%/%";
 
 -- Ne használd ki, hogy egy darab "-" vagy " " választja el egymástól a 7 jegyet!
 SELECT 
     vnev, telefonszam,
-    RIGHT(telefonszam, 
+    RIGHT(
+        telefonszam, 
         LOCATE("/", REVERSE(telefonszam)) - 1
     )
 FROM Dolgozok
 WHERE telefonszam LIKE "%/%";
 
+-- LOCATE: ha nincs benne a keresett szöveg, akkor 0 az értéke
+SELECT
+    vnev, telefonszam,
+    RIGHT(
+        telefonszam,
+        CHAR_LENGTH(telefonszam) - LOCATE("/", telefonszam)
+    ) AS szamjegyek
+FROM Dolgozok
+WHERE LOCATE("/", telefonszam) <> 0;
 
+SELECT
+    vnev, telefonszam,
+    SUBSTR(
+        telefonszam,
+        LOCATE("/", telefonszam) + 1
+    ) AS szamjegyek
+FROM Dolgozok
+WHERE LOCATE("/", telefonszam) <> 0;
 
 
 -- 5. Add meg a női dolgozók vezetéknevét!
 -- Minden név legyen egyedi!
-
+-- DISTINCT: egyedi rekordokat tart meg, SELECT-hez tartozik
+SELECT DISTINCT vnev
+FROM Dolgozok
+WHERE nem = "N";
 
 
 
@@ -74,16 +96,51 @@ WHERE telefonszam LIKE "%/%";
 -- melyik évben kezdtek a cégnél dolgozni?
 -- Jelenítsd meg a születési és a kezdési évszámot!
 -- Csak egyedi sorokat listázz!
+SELECT DISTINCT YEAR(szuletes) AS szuletes, YEAR(kezdes) AS kezdes
+FROM Dolgozok
+WHERE 1988 <= YEAR(szuletes) AND YEAR(szuletes) <= 1990;
 
 
-
+-- a <= x <= b
+-- a <= x AND x <= b
+-- x BETWEEN a AND b
 -- "Között" operátorral:
-
+SELECT DISTINCT YEAR(szuletes) AS szuletes, YEAR(kezdes) AS kezdes
+FROM Dolgozok
+WHERE YEAR(szuletes) BETWEEN 1988 AND 1990;
 
 
 
 
 -- 7. Sorold fel az email kiszolgálók neveit!
 -- Minden nevet csak egyszer jeleníts meg!
+-- Pl.: example.org
+SELECT DISTINCT SUBSTR(email, LOCATE("@", email) + 1) AS kiszolgalok
+FROM Dolgozok
+WHERE email IS NOT NULL;
 
+SELECT DISTINCT SUBSTR(email, LOCATE("@", email) + 1) AS kiszolgalok
+FROM Dolgozok
+WHERE SUBSTR(email, LOCATE("@", email) + 1) IS NOT NULL;
 
+SELECT DISTINCT SUBSTR(email, LOCATE("@", email) + 1) AS kiszolgalok
+FROM Dolgozok
+WHERE LOCATE("@", email) <> 0;
+
+SELECT DISTINCT SUBSTR(email, LOCATE("@", email) + 1) AS kiszolgalok
+FROM Dolgozok
+WHERE email LIKE "%@%";
+
+SELECT DISTINCT
+    RIGHT(
+        email,
+        CHAR_LENGTH(email) - LOCATE("@", email)
+    ) AS kiszolgalok
+FROM Dolgozok
+WHERE email LIKE "%@%";
+
+-- Rossz!
+-- SELECT DISTINCT SUBSTR(email, LOCATE("@", email) + 1) AS kiszolgalok
+-- FROM Dolgozok
+-- WHERE kiszolgalok IS NOT NULL;
+-- Vigyázat! Sorrend: FROM -> WHERE -> SELECT
