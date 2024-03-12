161736a5f8d2c9824a4950b0ea8a34a4b9a3aaee
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-12 12:25:49 +0100

    JOIN

---
+++ DQL/Többtábla használata/Naplo/naplo_halmazok.sql
@@ -0,0 +1,46 @@
+-- Halmaz műveletek:
+
+
+
+
+-- -----------------------------------------------
+-- 6. Kik azok a diákok (nev), akik kaptak már 3-mast
+-- valamilyen tárgyból? Mindegyikük nevét csak egyszer
+-- jelenítsük meg!
+
+
+
+
+-- 7. Kik azok a diákok, akik nem kaptak még 3-mast
+-- egyetlen tárgyból sem?
+
+
+
+
+-- 8. Kik azok a diákok (nev), akik webprogból
+-- és fizikából is kaptak már jegyet?
+
+
+
+
+-- 9. Jelenítsük meg:
+-- azokat a diákokat, akiknek nincs jegyük, továbbá
+-- azokat a jegyeket, amelyek nem tudjuk kihez tartoznak!
+-- Ezeken kívül semmilyen más adatot ne listázzunk!
+
+
+
+
+-- 10.
+-- a) Adjuk meg azokat a napló bejegyzéseket, amelyek
+-- esetén az osztályzat 5-ös (akkor is ha nem tudjuk, hogy
+-- melyik diákhoz tartozik)!
+-- b) A listát a diákok neve szerint abc sorrendben
+-- jelenítsük meg!
+-- c) Ne tartsuk meg azon rekordokat, ahol a diákról
+-- biztosan tudjuk, hogy 2000-ben született!
+-- d) Kerüljenek a lista végére azon rekordok, ahol
+-- nem ismerjük a diák nevét!
+
+
+
---
+++ DQL/Többtábla használata/Naplo/naplo_kapcsolatok.sql
@@ -0,0 +1,58 @@
+-- 1. Jelenítsük meg a jegyeket diakId helyett nevekkel!
+SELECT *
+FROM diak, jegy
+WHERE diak.id = jegy.diakId;
+
+SELECT *
+FROM diak d, jegy j
+WHERE d.id = j.diakId;
+
+-- "Másképp": JOIN
+SELECT *
+FROM diak d JOIN jegy j ON d.id = j.diakId;
+
+
+
+-- 2. Melyik diákok kaptak jegyet Matematika tárgyból?
+-- Osztályzat szerint csökkenő sorrendben írjuk ki!
+SELECT nev, targy, osztalyzat
+FROM diak d JOIN jegy j ON d.id = j.diakId
+WHERE targy = "Matematika"
+ORDER BY osztalyzat DESC;
+
+
+
+-- 3. Kik azok a diákok (id, nev),
+-- akik ugyanabban az évben születtek,
+-- mint egy másik diák társuk?
+SELECT DISTINCT d1.id, d1.nev
+FROM diak d1, diak d2
+WHERE YEAR(d1.datum) = YEAR(d2.datum) AND d1.id <> d2.id;
+
+
+
+-- 4.
+-- a) Töröljük a Naplóból, hogy kik kaptak Matematika tárgyból jegyet!
+--    Vigyázat: csak a diák azonosítóját "felejtsük el", ne a rekordot töröljük!
+-- b) Adjuk meg a teljes napló tartalmát újra!
+--    Vigyázat: legyenek a matek jegyek is közte (ott csak azt nem tudjuk, ki kapta).
+UPDATE Jegy
+SET diakId = NULL
+WHERE targy = "Matematika";
+
+SELECT *
+FROM diak d, jegy j
+WHERE d.id = j.diakId;
+
+-- "Belső" kapcsolat:
+
+
+-- "Külső" kapcsolatok:
+
+
+
+
+-- 5. És ha tényleg minden adat kell?
+-- => az a diák is aki nem kapott jegyet
+-- => az a jegy is, amit nem tudunk ki kapott
+
