01a5365d0f4a1b99d8c25405e12f562fb0f32eea
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-19 13:27:30 +0100

    UNION, INTERSECT, EXCEPT

--- DQL/JOIN/Naplo/naplo_halmazok.sql
+++ DQL/JOIN/Naplo/naplo_halmazok.sql
@@ -1,14 +1,35 @@
 -- Halmaz műveletek:
 
+-- a) Unio - legalább az egyikben benne van
+SELECT *
+FROM diak d RIGHT JOIN jegy j ON d.id = j.diakId
+UNION
+SELECT *
+FROM diak d LEFT JOIN jegy j ON d.id = j.diakId;
 
+-- b) Metszet - közös elemek
+SELECT *
+FROM diak d RIGHT JOIN jegy j ON d.id = j.diakId
+INTERSECT
+SELECT *
+FROM diak d LEFT JOIN jegy j ON d.id = j.diakId;
+
+-- c) Különbség - elsőben benne van, másodikban nincs
+-- Azok a jegyek, amiket nem tudjuk ki kapott:
+SELECT *
+FROM diak d RIGHT JOIN jegy j ON d.id = j.diakId
+EXCEPT
+SELECT *
+FROM diak d LEFT JOIN jegy j ON d.id = j.diakId;
 
 
 -- -----------------------------------------------
 -- 6. Kik azok a diákok (nev), akik kaptak már 3-mast
 -- valamilyen tárgyból? Mindegyikük nevét csak egyszer
 -- jelenítsük meg!
-
-
+SELECT *
+FROM diak INNER JOIN jegy ON diak.id = jegy.diakId
+WHERE osztalyzat = 3;
 
 
 -- 7. Kik azok a diákok, akik nem kaptak még 3-mast
